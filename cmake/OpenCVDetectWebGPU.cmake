35c0e1e3f375bc4fd1589857ead4cdb6ffa29afe
NALLEIN <hao1.jiang@intel.com>
2020-06-03 13:11:45 +0800

    Modify CMakeLists

--- cmake/OpenCVDetectWebGPU.cmake
+++ cmake/OpenCVDetectWebGPU.cmake
@@ -1,24 +1,25 @@
-if(HAVE_WEBGPU OR WITH_WEBGPU)
+ocv_clear_vars(HAVE_WEBGPU)
+if(WITH_WEBGPU)
   set(WEBGPU_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/3rdparty/include/webgpu/include")
   set(WEBGPU_LIBRARIES "${PROJECT_SOURCE_DIR}/3rdparty/include/webgpu/lib")
-endif()
-
-if(HAVE_WEBGPU OR WITH_WEBGPU)
-  include_directories(${WEBGPU_INCLUDE_DIRS})
-  link_directories(${WEBGPU_LIBRARIES})
-  link_libraries(dawn_proc dawn_native dawn_wire)
-
 endif()
 
 try_compile(VALID_WEBGPU
       "${OpenCV_BINARY_DIR}"
       "${OpenCV_SOURCE_DIR}/cmake/checks/webgpu.cpp"
-      CMAKE_FLAGS "-DINCLUDE_DIRECTORIES:STRING=${WEBGPU_INCLUDE_DIRS}"
+      CMAKE_FLAGS "-DINCLUDE_DIRECTORIES:STRING=${WEBGPU_INCLUDE_DIRS}" "-DLINK_LIBRARIES:STRING=${WEBGPU_LIBRARIES}"
       OUTPUT_VARIABLE TRY_OUT
       )
 if(NOT ${VALID_WEBGPU})
   message(WARNING "Can't use WebGPU-Dawn")
   return()
 endif()
+message(AUTHOR_WARNING "try_compile webgpu.cpp succeed")
 
 set(HAVE_WEBGPU 1)
+
+if(HAVE_WEBGPU)
+  include_directories(${WEBGPU_INCLUDE_DIRS})
+  link_directories(${WEBGPU_LIBRARIES})
+  link_libraries(dawn_proc dawn_native dawn_wire)
+endif()
